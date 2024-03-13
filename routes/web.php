e3d9257c3abf71b6623e5db02857ad450c409ba8
ARIRIFALDI <ahengyaya4@gmail.com>
2024-03-13 15:36:10 +0700

    first commit

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,17 @@
 <?php
-
+use App\Http\Controllers\Auth\AuthController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HaiController;
 
 
 Route::get('/postech/{nik}/cek',[HaiController::class,'index']);
 
-Route::get('/dasboard', function () {
-    return view('welcome');
-});
+Route::get('/dasboard', [AuthController::class, 'dasboard'])->name('dasboard');
+Route::get('login', [AuthController::class, 'index'])->name('login');
+Route::post('post-login', [AuthController::class, 'postLogin'])->name('login.post'); 
+Route::get('registration', [AuthController::class, 'registration'])->name('register');
+Route::post('post-registration', [AuthController::class, 'postRegistration'])->name('register.post'); 
+Route::get('logout', [AuthController::class, 'logout'])->name('logout');
+
+  
+
